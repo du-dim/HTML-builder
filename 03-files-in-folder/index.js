794a292f9725ef346ab1ba7e6b4638c830facf45
du-dim <dim-dubovik@yandex.ru>
2021-11-05 00:12:07 +0300

    fix: 03-files-in-folder/index.js

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,15 +3,17 @@ const path = require('path');
 
 let linkFile = path.join(__dirname, 'secret-folder');
 
-fs.readdir(linkFile, { withFileTypes: true }, (err, files) => {
-    if (err) throw err;
+fs.readdir(linkFile, { withFileTypes: true }, (error, files) => {
+    if (error) throw err;
     files.forEach(f => {
-        if (f.isFile()) {
-            fileName = f.name.split('.').slice(0, -1).join('.');       
-            extension = f.name.split('.').slice(-1).join('');
-            fileSize = fs.statSync(path.join(linkFile, f.name)).size/1000;
-            result = `${fileName} - ${extension} - ${fileSize}kb`;
-            console.log(result);
+        if (f.isFile()) {               
+            fs.stat(path.join(linkFile, f.name), (err, file) => {
+                if (err) throw err;
+                fileName = f.name.split('.').slice(0, -1).join('.');       
+                extension = f.name.split('.').slice(-1).join('');
+                result = `${fileName} - ${extension} - ${file.size/1000}kb`;
+                console.log(result);               
+            });            
         }
         else console.log(`Error: ${f.name} is a directory`); 
     });    
