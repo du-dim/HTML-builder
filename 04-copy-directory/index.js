7691a72455f5dd268a8576e4ae58bc6a032ecb91
du-dim <86885956+du-dim@users.noreply.github.com>
2021-11-10 12:50:33 +0300

    fix: code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,70 +1,26 @@
-const fs = require('fs');
+const fsp = require('fs').promises;
 const path = require('path');
+const fs = require('fs');
 
 let linkFile1 = path.join(__dirname, 'files');
 let linkFile2 = path.join(__dirname, 'files-copy');
 
-deleteFile(linkFile2);
-//deleteFolder(linkFile2);
-setTimeout(() => {copyDir(linkFile1, linkFile2)},1000);
+copyDir(linkFile1, linkFile2)
 
-function copyDir(a, b) {
-    fs.mkdir(b, { recursive: true }, (err) => {
-        if (err) throw err;    
-    });
-    fs.readdir(a, { withFileTypes: true }, (err, files) => {
-        if (err) throw err;
-        files.forEach(f => {
-            if (f.isFile()) {
-                let myReadStream = fs.createReadStream(path.join(a, f.name));
-                let myWriteStream = fs.createWriteStream(path.join(b, f.name));
-                myReadStream.pipe(myWriteStream);
-                myReadStream.on('error', (err) => console.log(err.message));
-                myWriteStream.on('error', (err) => console.log(err.message));
-            }
-            else copyDir(path.join(a, f.name), path.join(b, f.name));        
-        });          
-    });
+async function copyDir(a, b) {
+	const files = await fsp.readdir(a, { withFileTypes: true })
+	await fsp.mkdir(b, { recursive: true }) 
+	await fsp.rm(b, { recursive: true });
+	await fsp.mkdir(b, { recursive: true });
+	files.forEach(f => {
+		if (f.isFile()) {
+      let myReadStream = fs.createReadStream(path.join(a, f.name));
+			let myWriteStream = fs.createWriteStream(path.join(b, f.name));
+			myReadStream.pipe(myWriteStream);
+			myReadStream.on('error', (err) => console.log(err.message));
+			myWriteStream.on('error', (err) => console.log(err.message));
+    } else  {
+			copyDir(path.join(a, f.name), path.join(b, f.name)); 
+		}           
+  })
 }
-
-async function deleteFile(a) {
-    fs.readdir(path.join(a, '..'), (err, folders) => {
-        if (err) throw err;
-        let link = `${__dirname}\\`;
-        if (folders.includes(a.replace(link, '').trim())) {  
-            fs.readdir(a, { withFileTypes: true }, (err, files) => {
-                if (err) throw err;
-                files.forEach(f => {
-                    if (f.isFile()) {
-                        fs.rm(path.join(a, f.name), (err) => {
-                            if (err) throw err;
-                        })
-                    }
-                    else return deleteFile(path.join(a, f.name));        
-                });          
-            });
-        }  
-    });
-}
-/*
-async function deleteFolder(a) {
-    fs.readdir(a, { withFileTypes: true }, (err, dir) => {
-        console.log(dir.length);
-        if (err) throw err;        
-        if (dir.length > 0) {
-            dir.forEach(d => {
-                deleteFolder(path.join(a, d.name))
-            })
-        };
-        if (dir.length === 0) {
-            fs.rm(a, { recursive: true }, (err) => {
-                if (err) throw err;    
-            })
-        }   
-    });   
-}
-*/
-
-
-
-
